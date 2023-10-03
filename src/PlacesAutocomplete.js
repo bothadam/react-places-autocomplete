0b3ff3e7268f4eb94ecc4aeef3ceacb2c1d67842
bothadam <adam@linebooker.com>
2023-10-03 15:13:26 +0200

    wip - add default restriction to limit results to south africa

--- src/PlacesAutocomplete.js
+++ src/PlacesAutocomplete.js
@@ -107,6 +107,7 @@ class PlacesAutocomplete extends React.Component {
       this.autocompleteService.getPlacePredictions(
         {
           ...this.props.searchOptions,
+          componentRestrictions: { country: 'ZA' },
           input: value,
         },
         this.autocompleteCallback
